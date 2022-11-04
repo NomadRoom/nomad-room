f229e16912c69d148e464fc57bc379e60415a685
Lucas <lucasalex7235@gmail.com>
2022-11-04 08:47:38 -0300

    feat: add banner img

---
+++ src/styles/components/Search.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import banner from "../../assets/img/banner.png";
+
+export const StyledBannerSearch = styled.div`
+  width: 100vw;
+  height: 370px;
+  background-size: contain;
+  background: url(${banner});
+  /* background-repeat: no-repeat; */
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

14fedc0569c0725b2306bb4e4aefa4647940e4a0
Elfo <alefisantos15@gmail.com>
2022-11-07 09:27:44 -0300

    feat: add API request

--- src/components/ProvideRoom/formRoom.tsx
+++ src/components/ProvideRoom/formRoom.tsx
@@ -1,38 +1,66 @@
 import * as yup from "yup";
-import { useForm } from "react-hook-form";
+import {
+  DeepMap,
+  Message,
+  MultipleFieldErrors,
+  Ref,
+  useForm,
+} from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
 import { cepRequest } from "../../services/cep";
+import { StyledInput } from "../../styles/components/Input";
+import { api } from "../../services/api";
+
+interface iCep {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+}
+
+interface iForm {
+  title: string;
+  description: string;
+  gym: boolean;
+  internet: boolean;
+  tv: boolean;
+  garage: boolean;
+  pets: boolean;
+  furniture: boolean;
+  CEP: string;
+  neighborhood: string;
+  cityState: string;
+  street: string;
+  number: string;
+  photo: string;
+  contact: string;
+}
+
+export type FieldError = {
+  type: string;
+  ref?: Ref;
+  types?: MultipleFieldErrors;
+  message?: Message;
+};
+
+export type FieldErrors<TFieldValues extends iForm = iForm> = DeepMap<
+  TFieldValues,
+  FieldError
+>;
 
 export const FormRoom = () => {
+  const [neighborhood, setNeighborhood] = useState<string>("");
+  const [cityState, setCityState] = useState<string>("");
+  const [street, setStreet] = useState<string>("");
 
-  const [neighborhood, setNeighborhood] = useState('')
-  const [cityState, setCityState] = useState('')
-  const [street, setStreet] = useState('')
-  
-  interface iCep{
-    cep: string,
-    logradouro: string,
-    complemento: string,
-    bairro: string,
-    localidade: string,
-    uf: string,
-    ibge: string,
-    gia: string,
-    ddd: string,
-    siafi: string,
-  }
-
-  interface iData<i>{
-    data: i
-  }
-
-  const getExtension = (name: any) => {
-    let arr = name.split('.')
-    return arr.pop()
-  }
-
-  const regex: RegExp = /^\[1-9]{2}9[1-9][0-9]{3}[0-9]{4}$/
+  const regex: RegExp = /^\[1-9]{2}9[1-9][0-9]{3}[0-9]{4}$/;
 
   const schema = yup.object().shape({
     title: yup.string().required(),
@@ -48,50 +76,61 @@ export const FormRoom = () => {
     cityState: yup.string().required(),
     street: yup.string().required(),
     number: yup.string().required(),
-    photo: yup.mixed()
-    .test({
-      message: 'Insira um arquivo suportavel',
-      test: (file) => {
-        const isValid = ['png', 'jpg'].includes(getExtension(file?.name));
-        return isValid;
-      }
-    })
-    .test({
-      message: `arquivo muito grande, favor não ultrapassar 1024`,
-      test: (file) => {
-        const isValid = file?.size < 1024;
-        return isValid;
-      }
-    }),
+    photo: yup.string().required(),
     contact: yup.string().required().matches(regex),
-  })
+  });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<any>({
+  } = useForm<iForm>({
     resolver: yupResolver(schema),
   });
 
-  const onSubmitForm = () => {
-  
-  }
+  const onSubmitForm = async (data: iForm) => {
+    try {
+      await api.post("/register/room", {
+        title: data.title,
+        description: data.description,
+        about: {
+          gym: data.gym,
+          garage: data.garage,
+          internet: data.internet,
+          animals: data.pets,
+          tv: data.tv,
+          furnished: data.furniture,
+        },
+        location: {
+          postal_code: data.CEP,
+          district: data.neighborhood,
+          street: data.street,
+          city: data.cityState,
+          number: data.number,
+        },
+        room_img: data.photo,
+        contact: data.contact,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const cepListener = async (event: any) => {
-    if(event.target.value === 8){
-      try{
-        let response = await cepRequest.get<iCep>(`${event.target.value}/json`)
-        setNeighborhood(response.data.bairro)
-        setCityState(`${response.data.localidade}, ${response.data.uf}`)
-        setStreet(response.data.logradouro)
+    if (event.target.value === 8) {
+      try {
+        let response = await cepRequest.get<iCep>(`${event.target.value}/json`);
+        setNeighborhood(response.data.bairro);
+        setCityState(`${response.data.localidade}, ${response.data.uf}`);
+        setStreet(response.data.logradouro);
+      } catch (err) {
+        console.log(err);
       }
-      catch(err){console.log(err)}
     }
-  }
+  };
   return (
     <form onSubmit={handleSubmit(onSubmitForm)}>
-      <input type="text" {...register("title")} />
+      <StyledInput type="text" {...register("title")} />
       <textarea cols={30} rows={10} {...register("description")}></textarea>
       <div>
         <input type="checkbox" {...register("gym")} />
@@ -101,13 +140,18 @@ export const FormRoom = () => {
         <input type="checkbox" {...register("pets")} />
         <input type="checkbox" {...register("furniture")} />
       </div>
-      <input type="text" {...register("CEP")} onChange={cepListener} />
-      <input type="text" value={neighborhood} {...register("neighborhood")} />
-      <input type="text" value={cityState} {...register("cityState")} />
-      <input type="text" value={street} {...register("street")} />
-      <input type="text" {...register("number")} />
-      <input type="file" {...register("photo")} />
-      <input type="text" {...register("contact")} />
+      <StyledInput type="text" {...register("CEP")} onChange={cepListener} />
+      <StyledInput
+        type="text"
+        value={neighborhood}
+        {...register("neighborhood")}
+      />
+      <StyledInput type="text" value={cityState} {...register("cityState")} />
+      <StyledInput type="text" value={street} {...register("street")} />
+      <StyledInput type="text" {...register("number")} />
+      <StyledInput type="text" {...register("photo")} />
+      <StyledInput type="text" {...register("contact")} />
+      <button type="submit"></button>
     </form>
   );
 };

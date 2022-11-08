2574f301b7d621a3ea8cc38324d176b2af93efd9
Steiner <steinerst14@gmail.com>
2022-11-08 13:03:26 -0300

    feat: add roomPhoto

--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -1,3 +1,61 @@
-export const index = () => {
-  return <span>Apagar arquvio index.tsx</span>;
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../services/api";
+import { ContainerWrapper } from "../../styles/components/Container";
+import { Header } from "../Global/Header";
+import { RoomPhoto } from "./RoomPhoto";
+
+interface iRoom {
+  userId: number;
+  title: string;
+  description: string;
+  about: {
+    gym: boolean;
+    internet: boolean;
+    tv: boolean;
+    garage: boolean;
+    animals: boolean;
+    furnished: boolean;
+  };
+  localization: {
+    zip_code: string;
+    district: string;
+    state: string;
+    street: string;
+    number: number;
+  };
+  room_photo: string;
+  contact: number;
+  id: number;
+}
+
+export const Room = () => {
+  const [room, setRoom] = useState({} as iRoom);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getRoom = async () => {
+      const response = await api.get(`/rooms/${id}`);
+      const { data } = response;
+      console.log(data);
+      setRoom(data);
+    };
+
+    getRoom();
+  }, [id]);
+
+  return (
+    <>
+      {room.title ? (
+        <>
+          <Header />
+          <ContainerWrapper>
+            <RoomPhoto urlImg={room.room_photo} />
+          </ContainerWrapper>
+        </>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { ValidationPage } from "../pages/validationUser/indedx";
 import { Homepage } from "../components/Home";
+import { RoomPage } from "../pages/room";
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/validation" element={<ValidationPage />} />
-      <Route path='/' element={<Homepage/>}/>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/room/:id" element={<RoomPage />} />
     </Routes>
   );
 };

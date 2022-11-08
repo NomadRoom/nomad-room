d3a893028b8b58727f6ce2e10902f5b37e6695f4
JonathanVCB <viniciusjonathan@hotmail.com>
2022-11-08 09:59:20 -0300

    feat: fix request UserValidation on AuthContext

--- src/contexts/AuthContext/index.tsx
+++ src/contexts/AuthContext/index.tsx
@@ -8,10 +8,13 @@ export const AuthContext = createContext<iAuthValues>({} as iAuthValues);
 export const AuthProvider = ({ children }: iAuthProvider) => {
   const navigate = useNavigate();
   const UserValidation = async (data: iValidation): Promise<void> => {
+    const userId = localStorage.getItem("@NomadRoom:userId");
+    const token = localStorage.getItem("@NomadRoom:token");
     try {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       data = { ...data, validation: true };
-      // await api.patch(`/users/${userId}`, data);
-      navigate("/");
+      await api.patch(`/users/${userId}`, data);
+      navigate("/login");
     } catch (error) {
       console.log(error);
     }
